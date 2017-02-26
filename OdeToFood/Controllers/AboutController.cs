a7a6e9a05ce27e6455d041c62b69e35347d18770
Anton Savchenko <mrtihonya@gmail.com>
2017-02-26 22:25:29 +0200

    Example of attribute-based routing. (04_05 end)

--- OdeToFood/Controllers/AboutController.cs
+++ OdeToFood/Controllers/AboutController.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace OdeToFood.Controllers
 {
+    [Route("company/[controller]/[action]")]
     public class AboutController
     {
+       // [Route("")]
         public string Phone()
         {
             return "1+555-555-5555";
