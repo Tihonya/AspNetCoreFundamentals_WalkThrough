611953687c9ccf322c0858179345b5d96719cc97
Anton Savchenko <mrtihonya@gmail.com>
2017-02-26 03:54:59 +0200

    Play with Configuration Services(StartUp.cs).
    1)Added test class Greeter which takes role of Service.
    2)Registered class Greeter as service and parameter for it's
    constructor (IConfiguration) throw through the service collection as
    well.
    3)This is main pipeline in ASP.Net Core.

--- OdeToFood/Startup.cs
+++ OdeToFood/Startup.cs
@@ -14,7 +14,7 @@ namespace OdeToFood
     public class Startup
     {
 
-        public IConfiguration Configuration { get; set; }
+        private IConfiguration Configuration { get; set; }
         public Startup(IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
@@ -27,10 +27,16 @@ namespace OdeToFood
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(Configuration);
+            services.AddSingleton<IGreeter, Greeter>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(
+            IApplicationBuilder app,
+            IHostingEnvironment env,
+            ILoggerFactory loggerFactory,
+            IGreeter greeter)
         {
             loggerFactory.AddConsole();
 
@@ -42,7 +48,7 @@ namespace OdeToFood
             app.Run(async (context) =>
             {
 
-                var message = Configuration["Greeting"];
+                var message = greeter.GetGreeting();
                 await context.Response.WriteAsync(message);
             });
         }
